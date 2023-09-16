2e5418b631f0f7470cf2b32479e9dcf89fbd370e
nix <nix.loar@gmail.com>
2023-09-16 14:17:01 -0300

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render( <App />);
+
+reportWebVitals(console.log);
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+  {
+    entry: "./src/index.js",
+    
+    mode: "development",
+    
+    target: "web",
+    
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js"
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      }),
+    ],
+    
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          exclude: /node_modules/,
+          use: [
+              'style-loader', 
+              'css-loader',
+              'sass-loader',
+          ],
+        },
+      
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: ['@svgr/webpack'],
+        },
+
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          type: 'asset/resource',
+        },
+
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ]
+    }
+  },
+
+]
